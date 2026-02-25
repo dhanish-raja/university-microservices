e315f77d31b3ed2b4ad2422c90357f67f33bb38a
dhanish-raja <dhanishthecoder9398@gmail.com>
2026-02-26 00:02:21 +0530

    Add ApiGatewayApplication class for Spring Boot website

---
+++ backend/api-gateway/src/main/java/com/university/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.university.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayFinalApplication.class, args);
+	}
+
+}
